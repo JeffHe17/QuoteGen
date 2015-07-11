8614e9fd5f4259e703034d082c56604d19c14e8b
Jeffrey He <jhe12@terpmail.umd.edu>
2015-07-11 13:43:10 -0400

    script

--- js/script.js
+++ js/script.js
@@ -12,34 +12,35 @@ var usedBored = [];
 //Generates sad Quotes
 function sad(){
 	var index = random(0, sadQuotes.length - 1);
-	usedSad.push(index);
 
 	if(usedSad.length == sadQuotes.length){
 		usedSad = [];
 	}
 
-	if (usedSad.includes(index)){
+	if (usedSad.indexOf(index) == -1){
+		usedSad.push(index);
+		return sadQuotes[index];
+	} else {
 		sad();
 	}
-	alert(sadQuotes[index]);
-	return sadQuotes[index];
+
 }
 
 
 //Generates bored Quotes
 function bored(){
 	var index = random(0, boredQuotes.length - 1);
-	usedBored.push(index);
-
+	
 	if(usedBored.length == boredQuotes.length){
 		usedBored = [];
 	}
 
-	if (usedBored.includes(index)){
+	if (usedBored.indexOf(index) == -1){
+		usedBored.push(index);
+		return boredQuotes[index];
+	} else {
 		bored();
 	}
-	alert(boredQuotes[index]);
-	return boredQuotes[index];
 }
 
 
